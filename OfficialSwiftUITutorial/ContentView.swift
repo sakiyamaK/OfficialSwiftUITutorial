f8b23a4c9bf916d7178c027df6636fd57ff4023a
sakiyamaK <sakiyama.k@gmail.com>
2021-03-17 09:36:17 +0900

    Initial Commit

---
+++ OfficialSwiftUITutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OfficialSwiftUITutorial
+//
+//  Created by sakiyamaK on 2021/03/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OfficialSwiftUITutorial/OfficialSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfficialSwiftUITutorialApp.swift
+//  OfficialSwiftUITutorial
+//
+//  Created by sakiyamaK on 2021/03/17.
+//
+
+import SwiftUI
+
+@main
+struct OfficialSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
